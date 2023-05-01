fa123e8cd859d9e26641caadc58a514f3e36a35b
woejaddicor <your@emailadd.com>
2023-05-01 14:27:54 +0100

    resized single novie page

--- src/pages/SingleMovie/index.js
+++ src/pages/SingleMovie/index.js
@@ -15,27 +15,29 @@ export const SingleMovie = ({ addFavourite }) => {
 	}, [imdbID]);
 
 	return (
-		<div className={styles.container}>
-			<div className={styles.posterContainer}>
-				<img className={styles.poster} src={movie.Poster} alt=""></img>
-				<div className={styles.buttonContainer}>
-					<AddFavourite className={styles.addMovie} movie={movie} />
+		<div className={styles.singlePageBody}>
+			<div className={styles.container}>
+				<div className={styles.posterContainer}>
+					<img className={styles.poster} src={movie.Poster} alt=""></img>
+					<div className={styles.buttonContainer}>
+						<AddFavourite className={styles.addMovie} movie={movie} />
+					</div>
+					<p className={styles.imdbRating}>
+						IMDB Rating : {movie.imdbRating} ⭐️
+					</p>
+					<p className={styles.imdbRating}>Metascore : {movie.Metascore}</p>
 				</div>
-				<p className={styles.imdbRating}>
-					IMDB Rating : {movie.imdbRating} ⭐️
-				</p>
-				<p className={styles.imdbRating}>Metascore : {movie.Metascore}</p>
-			</div>
-			<div className={styles.info1}>
-				<h1 className={styles.movieTitle}>{movie.Title}</h1>
-				<p className={styles.info1}>Released : {movie.Released}</p>
-				<p className={styles.info1}>Runtime : {movie.Runtime}</p>
-				<p className={styles.info1}>Director : {movie.Director}</p>
-				<p className={styles.info1}>Genre : {movie.Genre}</p>
-				<p className={styles.info1}>Rated: {movie.Rated}</p>
-				<p className={styles.plot}>Plot Summary : '{movie.Plot}'</p>
-				<p className={styles.info1}>Cast : {movie.Actors}</p>
-				<p className={styles.info1}>Awards : {movie.Awards}</p>
+				<div className={styles.info1}>
+					<h1 className={styles.movieTitle}>{movie.Title}</h1>
+					<p className={styles.info1}>Released : {movie.Released}</p>
+					<p className={styles.info1}>Runtime : {movie.Runtime}</p>
+					<p className={styles.info1}>Director : {movie.Director}</p>
+					<p className={styles.info1}>Genre : {movie.Genre}</p>
+					<p className={styles.info1}>Rated: {movie.Rated}</p>
+					<p className={styles.plot}>Plot Summary : '{movie.Plot}'</p>
+					<p className={styles.info1}>Cast : {movie.Actors}</p>
+					<p className={styles.info1}>Awards : {movie.Awards}</p>
+				</div>
 			</div>
 		</div>
 	);
